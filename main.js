6457944c0594c566853fa57f18facb323e12c4cf
miloszKubiak <milypan69@gmail.com>
2021-02-26 21:07:10 +0100

    Added function for adding tasks

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 let $todoInput; //place, where the user enters the task content
-let $alertInfo // information about the lack of tasks / the need for text
+let $alertInfo; // information about the lack of tasks / the need for text
 let $addBtn; // the ADD button / adds items to the list
 let $ulList; // our to-do list, <ul></ul> tags
+let $newTask; //newly added LI, new task
 
 const main = () => {
     prepareDOMElements();
     prepareDOMEvents();
 }
 
-// we download our items
+//we download our items
 const prepareDOMElements = () => {
     $todoInput = document.querySelector('.todoInput');
     $alertInfo = document.querySelector('alertInfo');
@@ -17,6 +18,21 @@ const prepareDOMElements = () => {
 };
 
 //add event listeners
-const prepareDOMEvents = () => { };
+const prepareDOMEvents = () => {
+    $addBtn.addEventListener('click', addNewTask);
+ };
+
+const addNewTask = () => {
+    if ($todoInput.value !== '') {
+        $newTask = document.createElement('li');
+        $newTask.innerText = $todoInput.value;
+        $ulList.appendChild($newTask);
+
+        $todoInput.value = '';
+        $alertInfo.innerText = '';
+    } else {
+        $alertInfo.innerText = 'Enter the content of the task!'
+    }
+};
 
 document.addEventListener('DOMContendLoaded', main);
