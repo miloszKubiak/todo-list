1d31c818a34fdb71f4fb5c2e57f8f0133cb481bd
miloszKubiak <milypan69@gmail.com>
2021-02-26 21:29:49 +0100

    The function adding tasks has been fixed

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let $todoInput; //place, where the user enters the task content
-let $alertInfo; // information about the lack of tasks / the need for text
-let $addBtn; // the ADD button / adds items to the list
-let $ulList; // our to-do list, <ul></ul> tags
-let $newTask; //newly added LI, new task
+let todoInput; //place, where the user enters the task content
+let alertInfo; // information about the lack of tasks / the need for text
+let addBtn; // the ADD button / adds items to the list
+let ulList; // our to-do list, <ul></ul> tags
+let newTask; //newly added LI, new task
 
 const main = () => {
     prepareDOMElements();
@@ -11,28 +11,28 @@ const main = () => {
 
 //we download our items
 const prepareDOMElements = () => {
-    $todoInput = document.querySelector('.todoInput');
-    $alertInfo = document.querySelector('alertInfo');
-    $addBtn = document.querySelector('.addBtn');
-    $ulList = document.querySelector('.todoList ul');
+    todoInput = document.querySelector('.todoInput');
+    alertInfo = document.querySelector('alertInfo');
+    addBtn = document.querySelector('.addBtn');
+    ulList = document.querySelector('.todoList ul');
 };
 
 //add event listeners
 const prepareDOMEvents = () => {
-    $addBtn.addEventListener('click', addNewTask);
+    addBtn.addEventListener('click', addNewTask);
  };
 
 const addNewTask = () => {
-    if ($todoInput.value !== '') {
-        $newTask = document.createElement('li');
-        $newTask.innerText = $todoInput.value;
-        $ulList.appendChild($newTask);
+    if (todoInput.value !== '') {
+        newTask = document.createElement('li');
+        newTask.innerText = todoInput.value;
+        ulList.appendChild(newTask);
 
-        $todoInput.value = '';
-        $alertInfo.innerText = '';
+        todoInput.value = '';
+        alertInfo.innerText = '';
     } else {
-        $alertInfo.innerText = 'Enter the content of the task!'
+        alertInfo.innerText = 'Enter the content of the task!'
     }
 };
 
-document.addEventListener('DOMContendLoaded', main);
+document.addEventListener('DOMContentLoaded', main);
