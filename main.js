136acd519447881a7d8c24fee3b22ccb0aeac8a4
miloszKubiak <milypan69@gmail.com>
2021-02-27 15:00:09 +0100

    Added function that shows and hides the popup

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ let alertInfo; // information about the lack of tasks / the need for text
 let addBtn; // the ADD button / adds items to the list
 let ulList; // our to-do list, <ul></ul> tags
 let newTask; //newly added LI, new task
+let popup; //downloaded popup
+let popupInfo; //popup alert, when empty text is added
+let editedTodo; // edited Todo
+let popupInput; // text typed into input in the popup
+let addPopupBtn; // "ADD" button in popup
+let closeTodoBtn; // button to close the popup
+
 
 
 const main = () => {
@@ -16,12 +23,18 @@ const prepareDOMElements = () => {
     alertInfo = document.querySelector('.alertInfo');
     addBtn = document.querySelector('.addBtn');
     ulList = document.querySelector('.todoList ul');
+    popup = document.querySelector('.popup');
+    popupInfo = document.querySelector('.popupInfo'); 
+    popupInput = document.querySelector('.popupInput'); 
+    addPopupBtn = document.querySelector('.accept'); 
+    closeTodoBtn = document.querySelector('.cancel'); 
 };
 
 //add event listeners
 const prepareDOMEvents = () => {
     addBtn.addEventListener('click', addNewTask);
     ulList.addEventListener('click', checkClick);
+    closeTodoBtn.addEventListener('click', closePopup);
  };
 
 //adds new tasks
@@ -66,10 +79,18 @@ const checkClick = (e) => {
         e.target.closest('li').classList.toggle('completed');
         e.target.closest('button').classList.toggle('completed');
     } else if (e.target.closest('button').className === 'edit') {
-        console.log('edit');
+        editTask();
     } else if (e.target.closest('button').className === 'delete') {
         console.log('delete');
     }
 };
+
+const editTask = () => {
+    popup.style.display = 'flex';
+}
+
+const closePopup = () => {
+    popup.style.display = 'none';
+}
 
 document.addEventListener('DOMContentLoaded', main);
